9b6f9bd189f75cbdefd16c3ded4ed3f925efd974
katie026 <>
2023-03-10 02:10:21 -0600

    created loop from 1 to 100, but words are wrong

---
+++ fizzBuzz.py
@@ -0,0 +1,12 @@
+# Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+# For numbers which are multiples of both three and five print “FizzBuzz”
+
+for x in range(1, 101):
+    if x%3 == 1:
+        print("Fizz")
+    if x%5 == 1:
+        print("Buzz")
+    else:
+        print(x)
+    x += 1
