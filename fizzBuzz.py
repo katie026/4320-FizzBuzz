d1c5b44f7fdb9a900007419f71e4ae5a72ade183
katie026 <>
2023-03-10 02:31:02 -0600

    got it working with FizzBuzz word

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -6,7 +6,9 @@
 # A number is divisible by another number if it can be divided equally by that number; that is, if it yields a whole number when divided by that number. For example, 6 is divisible by 3 (we say "3 divides 6") because 6/3 = 2, and 2 is a whole number.
 
 for x in range(1, 101):
-    if x%3 == 0:
+    if x%3 == 0 and x%5 == 0:
+        print("FizzBuzz")
+    elif x%3 == 0:
         print("Fizz")
     elif x%5 == 0:
         print("Buzz")
