44fd043956bca8167c25e22593953b6121467897
katie026 <>
2023-03-10 03:07:35 -0600

    created functions including main, and added to description

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -2,12 +2,18 @@
 # But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
 # For numbers which are multiples of both three and five print “FizzBuzz”
 
-for x in range(1,101):
-    output = ""
-    if x % 3 == 0:
-        output = output + "Fizz"
-    if x % 5 == 0:
-        output = output + "Buzz"
-    if x % 5 != 0 and x % 3 != 0:
-        output = output + str(x)
-    print(output)
+def fizzBuzz(min, max):
+    for x in range(min, max+1):
+        output = ""
+        if x % 3 == 0:
+            output = output + "Fizz"
+        if x % 5 == 0:
+            output = output + "Buzz"
+        if x % 5 != 0 and x % 3 != 0:
+            output = output + str(x)
+        print(output)
+
+def main():
+    fizzBuzz(1, 100)
+
+main()
